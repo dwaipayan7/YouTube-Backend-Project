aac5983c33f8937d3640c4f793c1fb938c9ccdc4
Dwaipayan <biswastatay73@gmail.com>
2024-05-24 03:32:01 +0530

    day 2nd week

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
 
 const userSchema = new Schema({
     username: {
@@ -56,10 +58,43 @@ const userSchema = new Schema({
 )
 
 userSchema.pre("save", async function(next){
+    if (!this.isModified("password")) {
+        
+        return next();
+    }
     this.password = bcrypt.hash(this.password, 10)
     next()
 })
 
+
+userSchema.methods.isPasswordCorrect = async function(password){
+   return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccessToken = function(){
+   return jwt.sign({
+        __id: this.__id,
+        email:this.email,
+        username: this.username,
+        fullname: this.fullname
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+        expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+    }
+)
+}
+
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign({
+        __id: this.__id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+        expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+    }
+)
+}
 
 
 
