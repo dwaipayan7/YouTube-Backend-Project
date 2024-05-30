153bc65aafd140d0ea4270e285240e7ab79a8204
Dwaipayan <biswastatay73@gmail.com>
2024-05-31 02:08:46 +0530

    initial

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,10 +1,38 @@
 import {asyncHandler} from "../utils/asyncHandler";
+import {ApiError} from "../utils/ApiError.js"
 
 
 const registerUser = asyncHandler( async (req, res) =>{
-    res.status(200).json({
-        message: "ok"
-    })
+   // get user details from frontend
+   // validation
+   //check if user already exist
+   // check for images
+   // upload cloudnary , avatar
+    // create object
+
+    //check for user creation
+    // return res
+
+    const {fullName, email, username, password} = req.body
+    
+        console.log("email: ",email);
+
+        // if (fullName === "") {
+        //     throw new ApiError(400, "fullname is required")
+
+        // }
+
+        if (
+            [fullName, email, username, password].some((field) => 
+            
+            field?.trim() === "")
+        ) {
+
+            throw new ApiError(400, "All field is required")
+            
+        }
+
+
 })
 
 export {registerUser}
