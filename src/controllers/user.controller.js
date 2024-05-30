0db43091f8e05e04ba7fcf323c3bd44b20c7cd38
Dwaipayan <biswastatay73@gmail.com>
2024-05-31 02:29:55 +0530

    next

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,5 +1,7 @@
 import {asyncHandler} from "../utils/asyncHandler";
 import {ApiError} from "../utils/ApiError.js"
+import {Uesr, User} from "../models/user.model.js"
+import {uploadOnCloudinary} from "../utils/cloudinary.js"
 
 
 const registerUser = asyncHandler( async (req, res) =>{
@@ -31,6 +33,38 @@ const registerUser = asyncHandler( async (req, res) =>{
             throw new ApiError(400, "All field is required")
             
         }
+        
+      const existedUser =  Uesr.findOne({
+            $or: [{username}, {email}]
+        })
+
+        if (existedUser) {
+            throw new ApiError(400, "User with email or username already exists")
+        }
+
+     const avatarLocalPath = req.files?.avatar[0]?.path;
+     const coverImageLocalPath = req.files.coverImage[0]?.path;
+
+     if (!avatarLocalPath) {
+        throw new ApiError(400, "Avatar file is required")
+     }
+
+   const avatar = await uploadOnCloudinary(avatarLocalPath)
+   const coverImage =  await uploadOnCloudinary(coverImageLocalPath)
+
+     if (!avatar) {
+        throw new ApiError(400, "Avatar file is required")
+     }
+
+     User.create({
+        fullName,
+        avatar: avatar.url,
+        coverImage: coverImage?.url || "",
+        email,
+        password,
+        username: username.toLowerCase()
+     })
+
 
 
 })
